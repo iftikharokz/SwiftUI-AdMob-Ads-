1f1ea3facacffff6ca30af8cf971542378efb69f
TheAppMedia <iftikharhussain2230@gmail.com>
2022-09-30 09:45:51 +0500

    Initial Commit

---
+++ SwiftUIAdMob Ads/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAdMob Ads
+//
+//  Created by Theappmedia on 9/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAdMob Ads/SwiftUIAdMob_AdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdMob_AdsApp.swift
+//  SwiftUIAdMob Ads
+//
+//  Created by Theappmedia on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdMob_AdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
